eb79a99f079186ad4bafb9022c01a84b9ce47611
Dimitris Tsapnidis <dimitrios.tsapnidis@gmail.com>
2022-08-26 13:02:12 +0300

    Rename runner.py to remote.py ;

--- remote.py
+++ remote.py
@@ -1,154 +1,145 @@
-"""Package to run remote commands."""
-from time import sleep
-import paramiko
+import argparse
+import asyncio
+import json
 import logging
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+import asyncssh
 
 
-class Connection:
+def parse_args():
+    """Parse the script arguments.
 
-    def __init__(self, host, port, username, password):
-        self.host = host
-        self.port = port
-        self.username = username
-        self.password = password
+    :return: The parsed args
+    :rtype: argparse.Namespace
+    """
+    parser = argparse.ArgumentParser(description="Run command on a remote.")
+    parser.add_argument("command", help="The command to run")
+    parser.add_argument("-r", "--remote", help="The remote name", required=False)
+    parser.add_argument("-i", "--host", help="The remote hostname", required=False)
+    parser.add_argument("-p", "--port", type=int, help="The remote port", required=False)
+    parser.add_argument("-u", "--username", help="The remote username", required=False)
+    parser.add_argument("-w", "--password", help="The remote password", required=False)
+    parser.add_argument("-l", "--log-level", help="The logging level", choices=["INFO", "ERROR", "DEBUG"], default="INFO")
+    parser.add_argument("--log-file", default=f"{Path(__file__).stem}.log", help="The logging file")
+    parser.add_argument("--error-log-file", default=f"{Path(__file__).stem}.error.log", help="The error logging file")
+    args = parser.parse_args()
+    if args.remote and any([args.host, args.port, args.username, args.password]):
+        parser.error("Only one of the remote and custom remote can be specified.")
+    if not args.remote and not all([args.host, args.port, args.username, args.password]):
+        parser.error("All custom remote fields should be specified.")
+    return args
 
-
-def get_default_logger():
-    logger = logging.getLogger()
-    stream_handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
-    logger.setLevel("DEBUG")
-    return logger
 
 
 class Remote:
+    """A remote machine.
+    
+    :param str host: The remote host
+    :param int port: The remote port
+    :param str username: The remote username
+    :param str password: The remote password
+    :param logging.Logger logger: The logger object
+    """
 
-    def __init__(self, host, port, username, password, logger):
-        self._connection = Connection(host, port, username, password)
-        self._logger = logger or get_default_logger()
-        self._client = paramiko.SSHClient()
+    def __init__(self, host, port, user, password, logger):
+        self._host = host
+        self._port = port
+        self._user = user
+        self._password = password
+        self._logger = logger
 
-    def connect(self):
+    async def async_run_command(self, command):
+        """Run command asynchronously.
+
+        :param str command: The command to run
         """
-        Connect to the remote host.
+        async with asyncssh.connect(host=self._host, port=self._port, username=self._user, password=self._password) as connection:
+            result = await connection.run(command)
+            self._logger.info(result)
+            return result.stdout, result.stderr
 
-        :param kwargs: Other parameters for the paramiko.SSHClient `connect` method.
+    def run(self, command):
+        """Run command.
+
+        :param str command: The command to run
         """
-        if not self.is_connected():
-            self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-            self._logger.debug(f"Connecting on {self._connection.host}...")
-            self._client.connect(
-                hostname=self._connection.host, 
-                port=self._connection.port,
-                username=self._connection.username,
-                password=self._connection.password
-            )
+        return asyncio.get_event_loop().run_until_complete(
+            self.async_run_command(command)
+        )
 
-    def is_connected(self):
-        return self._client.get_transport() is not None and self._client.get_transport().is_active()
 
-    def disconnect(self):
+class NamedRemote(Remote):
+
+    def __init__(self, name, logger):
+        remote_data = self.get(name)
+        if not remote_data:
+            raise Exception("Remote doesn't exist!")
+        super().__init__(remote_data["host"], remote_data["port"], remote_data["user"], remote_data["password"], logger)
+    
+    def read_remotes(self):
+        """Read remotes.
+
+        :return: The remotes
+        :rtype: list[dict]
         """
-        Disconnect from the remote host.
+        with open("remotes.json") as fp:
+            return json.load(fp)
+
+
+    def get(self, name):
+        """Return remote by name.
+
+        :param str name: The remote name
+        :return: The remote data
+        :rtype: dict
         """
-        if self._client:
-            self._logger.debug(f"Disconnecting from {self._connection.host}...")
-            return self._client.close()
+        return self.read_remotes().get(name, None)
 
-    @classmethod
-    def _write_stream(cls, stream, text):
-        """
-        Write text to stream.
 
-        :param stream: The stream to write to.
-        :param text: The text to write.
-        """
-        if not text:
-            return
-        stream.write(text)
-        stream.flush()
-        sleep(1)
+def setup_logger(level, log_file, error_log_file):
+    """Set up a logger.
 
-    @classmethod
-    def _read_stream(cls, stream):
-        """
-        Read from stream.
+    :param str level: The log level
+    :param str log_file: The log file
+    :param str error_log_file: The error log file
+    :return: The logger
+    :rtype: logging.Logger
+    """
+    logger = logging.getLogger(__name__)
+    file_handler = RotatingFileHandler(
+        log_file,
+        maxBytes=10000000,
+        backupCount=0
+    )
+    error_file_handler = RotatingFileHandler(
+        error_log_file,
+        maxBytes=10000000,
+        backupCount=0
+    )
+    stream_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+    file_handler.setFormatter(formatter)
+    error_file_handler.setFormatter(formatter)
+    error_file_handler.setLevel("ERROR")
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger.addHandler(error_file_handler)
+    logger.addHandler(stream_handler)
+    logger.setLevel(level)
+    return logger
 
-        :param stream: The stream to read from.
-        :return: The stream contents.
-        """
-        stream.flush()
-        return stream.read().decode('utf-8')
 
-    def sudo(self, command, input_text=None):
-        """
-        Run command with sudo.
-
-        :param command: The command to run.
-        :param password: The password of the sudo user.
-        :param input_text: The input text of the command (if needed by the command).
-        :param kwargs: Other parameters of paramiko.SSHClient() `exec_command` method.
-        :return: The output and the error of the command.
-        """
-        self.connect()
-
-        if not command.startswith("sudo"):
-            command = "sudo -S -p '' " + command
-
-        self._logger.info(command)
-        stdin, stdout, stderr = self._client.exec_command(command)
-
-        if self._connection.password and not self._connection.password.endswith('\n'):
-            password = '{}\n'.format(self._connection.password)
-
-        self._write_stream(stdin, password)
-
-        if input_text is not None:
-            self._write_stream(stdin, input_text)
-
-        stdin.channel.shutdown_write()
-        output = self._read_stream(stdout)
-        error = self._read_stream(stderr)
-        self._logger.info(output)
-        if error:
-            self._logger.error(error)
-        return output, error
-
-    def run(self, command, input_text=None, sudo=False):
-        """
-        Run command.
-
-        :param command: The command to run.
-        :param input_text: The input text (if needed by the command).
-        :param sudo: Determine to run with sudo.
-        :param password: The sudo password (if sudo is true).
-        :param kwargs: Other parameters of paramiko.SSHClient() `exec_command` method.
-        :return: The output and the error of the command.
-        """
-        self.connect()
-
-        if sudo:
-            return self.sudo(command=command, input_text=input_text)
-
-        self._logger.info(command)
-        stdin, stdout, stderr = self._client.exec_command(command)
-
-        if input_text is not None:
-            self._write_stream(stdin, input_text)
-
-        stdin.channel.shutdown_write()
-        output = self._read_stream(stdout)
-        error = self._read_stream(stderr)
-        self._logger.info(output)
-        if error:
-            self._logger.error(error)
-        return output, error
-
-    def __del__(self):
-        """ Called when the instance is about to be destroyed. 
-                
-        The connection has to be closed here.
-        """
-        self.disconnect()
+if __name__=="__main__":
+    args = parse_args()
+    remote = NamedRemote(
+        args.remote, 
+        setup_logger(args.log_level, args.log_file, args.error_log_file)
+    ) if args.remote else Remote(
+        args.host,
+        args.port,
+        args.username,
+        args.password,
+        setup_logger(args.log_level, args.log_file, args.error_log_file)
+    )
+    remote.run(args.command)
